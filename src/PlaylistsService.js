08c3aab3911e1320e226f1ef62ff975061a67e1c
PetengDedet <pueteng.dedet@gmail.com>
2021-07-09 14:45:49 +0700

    Init

---
+++ src/PlaylistsService.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+
+class PlaylistsService {
+  constructor() {
+    this._pool = new Pool();
+  }
+
+  async getPlaylistSongs(playlistId) {
+    const query = {
+      text: `SELECT s.id, s.title, s.performer
+        FROM playlistsongs ps
+        LEFT JOIN songs s ON ps.song_id = s.id
+        WHERE playlist_id = $1`,
+      values: [playlistId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return result.rows;
+  }
+}
+
+module.exports = PlaylistsService;
